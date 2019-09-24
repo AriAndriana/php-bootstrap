d4bf4fdeb02e29d3533290b120b9af5af231c524
abumilhan78 <abumilhan78@gmail.com>
2019-09-24 11:44:45 +0700

    bootstrap php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Page Title</title>
+    <!-- Pemanggilan CSS -->
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <!-- Akhir Pemanggilan CSS -->
+</head>
+<body>
+    <!-- Header -->
+    <!-- End Header -->
+
+    <!-- Content --> 
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12" style="background:violet">
+                <h1>12 Columns</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6" style="background:red">
+                <h1>6 Columns</h1>
+            </div>
+            <div class="col-md-6" style="background:blue">
+                <h1>6 Columns</h1>
+            </div>
+        </div>
+    </div>
+    <!-- End Content --> 
+
+    <!-- Footer --> 
+    <!-- End Footer --> 
+
+    <!-- Pemanggilan JS -->
+    <script src="/assets/js/bootstrap.bundle.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>    
+</body>
+</html>
